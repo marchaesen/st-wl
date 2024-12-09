5f1ccabeee6d175fe5f0039161e36bf07dd2023e
Marc Haesen <Marc.Haesen@icareweb.com>
2024-12-09 08:57:33 +0100

    NO_WINDOW_DECORATIONS_PATCH implemented

--- wl.c
+++ wl.c
@@ -2249,8 +2249,10 @@ wlinit(int cols, int rows)
 	xdg_toplevel_add_listener(wl.xdgtoplevel, &xdgtoplevellistener, NULL);
 	xdg_toplevel_set_app_id(wl.xdgtoplevel, opt_class ? opt_class : termclass);
 
+#if !NO_WINDOW_DECORATIONS_PATCH
   struct zxdg_toplevel_decoration_v1 *decoration = zxdg_decoration_manager_v1_get_toplevel_decoration(wl.decoration_manager, wl.xdgtoplevel);
   zxdg_toplevel_decoration_v1_set_mode(decoration, ZXDG_TOPLEVEL_DECORATION_V1_MODE_SERVER_SIDE);
+#endif
 
 	wl_surface_commit(wl.surface);
 
